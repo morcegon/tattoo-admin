ab931a5ceb8499c290f4cd6f8d5e77dddc62fafb
Renan Andrade <eu@renanandrade.com.br>
2020-09-12 17:31:24 -0300

    chore: setup typescript

---
+++ src/controllers/users.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from 'express'
+
+const users = [
+  {
+    name: 'Renan',
+    email: 'eu@renanandrade.com.br',
+  },
+]
+
+const index = (req: Request, res: Response) => res.json(users)
+
+export default { index }
